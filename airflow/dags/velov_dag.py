135960de6c205ca243305fb0ea2b4395accc829f
TimotheChiesi <timothe.chiesi@etu.mines-ales.fr>
2026-02-06 11:15:43 +0100

    fix: error with dag parameter to start automatically

--- airflow/dags/velov_dag.py
+++ airflow/dags/velov_dag.py
@@ -24,7 +24,7 @@ with DAG(
     description='Fetch Velo\'v station data every 5 minutes',
     schedule_interval='*/5 * * * *',  # Every 5 minutes
     start_date=datetime(2024, 1, 1),
-    is_paused_at_creation=False,
+    is_paused_upon_creation=False,
     catchup=False,
 ) as dag:
 
